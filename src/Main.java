7fe79df10273b30df9dbfa1ddf53febba6efe115
Zack01234 <limonod57@gmail.com>
2022-09-06 10:15:34 +0500

    первый коммит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
 
+        //Задание 1
+        int year = 2001;
+        printMessageLeapYear(leapYear(year), year);
+
+        //Задание 2
+    }
+
+    public static boolean leapYear(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void printMessageLeapYear(boolean leapYear, int year) {
+        if (leapYear) {
+            System.out.println(year + " — високосный год.");
+        } else {
+            System.out.println(year + " — не високосный год.");
+        }
     }
 }
